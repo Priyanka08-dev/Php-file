07cc2eb2bc5e2d5dd4611d32d7227fcca6a35d05
Priyanka08-dev <135977762+Priyanka08-dev@users.noreply.github.com>
2023-06-08 18:37:31 +0530

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+        content="width=device-width,initial-scale=1.0">
+        <title>
+            PHP-HELLO WORLD</title>
+        <body>
+            <h1>
+                <?php echo'hello world';?>
+            </h1>
+        </body>
+    </head>
+</html>
